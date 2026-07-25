2576aec03ce852c367e7c2e8ff4f8ba9a8833e82
Pavel Busovikov <pavel.busovikov@gmail.com>
2026-07-25 11:41:48 +0700

    Bring the playground action into view

--- apps/playground/src/entry-scene-assets.test.ts
+++ apps/playground/src/entry-scene-assets.test.ts
@@ -82,7 +82,7 @@ describe('playground entry scene', () => {
       -2 * (qy * qz - qw * qx),
       -(1 - 2 * (qx * qx + qy * qy)),
     ];
-    const toPlayArea = [0 - px, 10 - py, 5 - pz];
+    const toPlayArea = [0 - px, 3 - py, 5 - pz];
     const length = Math.hypot(...toPlayArea);
     const alignment =
       (forward[0]! * toPlayArea[0]! +
